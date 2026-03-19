d1c2d1de350f915f31b00fc92d695271b44c674e
romeperotti2990 <meepug0@gmail.com>
2026-03-19 09:44:41 -0600

    routing/mod04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Departments } from './components/departments/departments'
+import { Timesheet } from './components/timesheet/timesheet';
+import { Analytics } from './components/analytics/analytics';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'departments', pathMatch: 'full' },
+    { path: 'departments', component: Departments },
+    { path: 'timesheet', component: Timesheet },
+    { path: 'analytics', component: Analytics }
+];
--- src/app/modules/material-module.ts
+++ src/app/modules/material-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,11 +10,13 @@ import { MatIconModule } from '@angular/material/icon';
   declarations: [],
   imports: [
     CommonModule,
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
   ],
   exports: [
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule
