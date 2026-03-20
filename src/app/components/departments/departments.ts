9ee4cddafa4ace0d18d5bc561ece9b0a93914254
romeperotti2990 <meepug0@gmail.com>
2026-03-20 08:08:14 -0600

    more routing/mod6

--- src/app/components/departments/departments.ts
+++ src/app/components/departments/departments.ts
@@ -3,6 +3,7 @@ import { DepartmentsService } from '../../services/departments';
 import { Department } from '../../interfaces/department';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../modules/material-module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -14,9 +15,16 @@ import { MaterialModule } from '../../modules/material-module';
 export class Departments implements OnInit {
   departments!: Department[];
 
-  constructor(private departmentsService: DepartmentsService) { }
+  constructor(
+    private departmentsService: DepartmentsService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
+  }
+
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['/timesheet', { id: departmentId }]);
   }
 }
--- src/app/components/timesheet/timesheet.ts
+++ src/app/components/timesheet/timesheet.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments';
+import { Department } from '../../interfaces/department';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../modules/material-module';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-timesheet',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
   templateUrl: './timesheet.html',
-  styleUrl: './timesheet.scss',
+  styleUrls: ['./timesheet.scss']
 })
-export class Timesheet {}
+export class Timesheet implements OnInit {
+  department!: Department;
+  departments!: Department[];
+
+  constructor(
+    private departmentsService: DepartmentsService,
+    private route: ActivatedRoute // <-- New
+  ) { }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(
+      (department) => department.id === this.route.snapshot.params['id']
+    )!;
+  }
+}
