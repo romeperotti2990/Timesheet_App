f653efd2aff139ef9f7abff8b5e15e8eabe0df48
romeperotti2990 <meepug0@gmail.com>
2026-03-20 08:45:03 -0600

    added reactive input/mod7

--- src/app/components/timesheet/timesheet.ts
+++ src/app/components/timesheet/timesheet.ts
@@ -1,30 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { DepartmentsService } from '../../services/departments';
 import { Department } from '../../interfaces/department';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../../modules/material-module';
+import { DepartmentsService } from '../../services/departments';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
+import { MaterialModule } from '../../modules/material-module';
+import { Employee } from '../../interfaces/employee';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-timesheet',
-  standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [MaterialModule, JsonPipe],
   templateUrl: './timesheet.html',
-  styleUrls: ['./timesheet.scss']
+  styleUrl: './timesheet.scss'
 })
 export class Timesheet implements OnInit {
+  
   department!: Department;
   departments!: Department[];
+  employeeNameFC = new FormControl('', this.nameValidator()); // <-- New FormControl Instance
+  employees: Employee[] = [];
+  employeeId = 0;
 
-  constructor(
-    private departmentsService: DepartmentsService,
-    private route: ActivatedRoute // <-- New
-  ) { }
+  constructor(private departmentsService: DepartmentsService,
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(
       (department) => department.id === this.route.snapshot.params['id']
     )!;
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id, // Note the use of the ? operator
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50, // Assigns a random pay rate
+      });
+
+      // Clear the input field after successful submission
+      this.employeeNameFC.setValue('');
+    }
+  }
+
+  nameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      // Validation logic
+      let error = null;
+      if (this.employees && this.employees.length) {
+        this.employees.forEach(employee => {
+          // Compare lowercase input value with existing employee names
+          if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+            error = { duplicate: true }; // Found a duplicate
+          }
+        });
+      }
+      return error; // Returns {duplicate: true} on failure, or null on success
+    };
   }
 }
--- src/app/modules/material-module.ts
+++ src/app/modules/material-module.ts
@@ -1,32 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; // not there in the tutorial
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card'; 
-import { MatListModule } from '@angular/material/list'; 
-
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule,
+    RouterModule, // not there in the tutorial
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   exports: [
-    RouterModule,
+    RouterModule, // not there in the tutorial
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ]
 })
 export class MaterialModule { }
