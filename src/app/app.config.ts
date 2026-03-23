cffcf2c5bbf8c13b5ea15b2c5d316ecbdf9d14de
romeperotti2990 <meepug0@gmail.com>
2026-03-23 09:03:52 -0600

    firebase/mod13

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // <-- New Import
 
 import { routes } from './app.routes';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient() // <-- New
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBMFF2VCgLlU1KDC1W53STyLuSEykHMStE",
+        authDomain: "timesheetapp-c0de4.firebaseapp.com",
+        projectId: "timesheetapp-c0de4",
+        storageBucket: "timesheetapp-c0de4.firebasestorage.app",
+        messagingSenderId: "659661804239",
+        appId: "1:659661804239:web:1710c1c60ae4d7430d1dba",
+        measurementId: ''
+    },
+};
